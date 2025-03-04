9bdc3bfaaaff1a529587922c0c7b807caad6cd73
Yong Tang <yong.tang.github@outlook.com>
2025-03-04 01:30:10 +0000

    MinimalPublisher

--- motion_agent/agent.py
+++ motion_agent/agent.py
@@ -1,6 +1,39 @@
-def main():
-    print('Hi from motion_agent.')
+import rclpy
+from rclpy.node import Node
+
+from std_msgs.msg import String
 
 
-if __name__ == '__main__':
+class Agent(Node):
+
+    def __init__(self):
+        super().__init__("agent")
+        self.publisher_ = self.create_publisher(String, "topic", 10)
+        timer_period = 0.5  # seconds
+        self.timer = self.create_timer(timer_period, self.timer_callback)
+        self.i = 0
+
+    def timer_callback(self):
+        msg = String()
+        msg.data = "Hello World: %d" % self.i
+        self.publisher_.publish(msg)
+        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.i += 1
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    agent = Agent()
+
+    rclpy.spin(agent)
+
+    # Destroy the node explicitly
+    # (optional - otherwise it will be done automatically
+    # when the garbage collector destroys the node object)
+    agent.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
     main()
